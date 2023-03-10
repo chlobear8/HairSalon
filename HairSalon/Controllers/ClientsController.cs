712d6ad7a9d02c104c6f0bd7281b3934fce46440
Chloe ONeil <chloesporri@yahoo.com>
2023-03-10 14:00:46 -0800

    add edit route to Clients controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -38,5 +38,19 @@ namespace HairSalon.AddControllers
       Client thisClient = _db.Clients.FirstorDefault(client => client.ClientId == id);
       return View(thisClient);
     }
+
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstorDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
