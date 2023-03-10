e1f23e0f58b93d40223ea0ea092a09b137a6a1f7
Chloe ONeil <chloesporri@yahoo.com>
2023-03-10 14:26:24 -0800

    add Stylist.cs Model

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -5,6 +5,7 @@ namespace HairSalon.Models
   public class HairSalonContext : DbContext
   {
     public DbSet<Client> Clients { get; set; }
+    public DbSet<Stylist> Stylists { get; set; }
 
   public HairSalonContext(DbContextOptions options) : base(options) { }
   }
