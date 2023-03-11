6286112e91e68382102851e53e627e1ed963b9fa
Chloe ONeil <chloesporri@yahoo.com>
2023-03-11 10:57:10 -0800

    adjust Program.cs BUGFIX

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -1,41 +1,29 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using HairSalon.Models;
-using System.Linq;
 
-namespace HairSalon
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-      builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();
 
-      builder.Services.AddDbContext<HairSalonContext>(
-                        dbContextOptions => dbContextOptions
-                          .UseMySql(
-                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                          )
-                        )
-                      );
+builder.Services.AddDbContext<HairSalonContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
 
-      WebApplication app = builder.Build();
+WebApplication app = builder.Build();
 
-      app.UseHttpsRedirection();
+// app.UseDeveloperExceptionPage();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-      app.UseStaticFiles();
+app.UseRouting();
 
-      app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-      app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
-
-      app.Run();
-    }
-  }
-}
+app.Run(); 
