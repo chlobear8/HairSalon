9268649dbcef19960f4716616ca8f3c9e2514397
Chloe ONeil <chloesporri@yahoo.com>
2023-03-16 22:07:02 -0700

    add error handling for stylist existing before adding client

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HairSalon.Models
 {
@@ -6,6 +7,7 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage= "You must have a stylist to add a client to. Have you inputted a stylist yet?")]
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
   }
